cf18f2d4152dbe2d116b9a58e6a62c1f20ea3d13
LiTengko <85880935+LiTengko@users.noreply.github.com>
2022-11-08 11:08:37 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/LiTengko/nextjs-notion-starter-kit'
+      href='https://github.com/LiTengko'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
